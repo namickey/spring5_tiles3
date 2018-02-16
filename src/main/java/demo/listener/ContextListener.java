19ffbc48c070529eebaf531ac5d05da5e189a806
namickey <teethdai@gmail.com>
2018-02-16 23:21:43 +0900

    add listener.

---
+++ src/main/java/demo/listener/ContextListener.java
@@ -0,0 +1,17 @@
+package demo.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class ContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("contextInitialized..");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        System.out.println("contextDestroyed..");
+    }
+}
