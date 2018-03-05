346bc71ac390470c168eb78ffd6f151c6d649417
namickey <teethdai@gmail.com>
2018-03-06 00:47:21 +0900

    add interceptor

---
+++ src/main/java/demo/interceptor/LogInterceptor.java
@@ -0,0 +1,15 @@
+package demo.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogInterceptor extends HandlerInterceptorAdapter{
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        System.out.println("*********************");
+        return true;
+    }
+}
